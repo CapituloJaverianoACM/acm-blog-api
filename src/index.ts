59f77036fdbd7a792cecfb39dff3f7c0d698b367
Juan Canon <canonjp@javeriana.edu.co>
2025-09-01 14:57:23 -0500

    feat: method disconnect() now shutdowns gracefuly the server and database to ensure good practices

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Elysia } from "elysia";
 import { swagger } from "@elysiajs/swagger";
 import { swaggerConfig, welcomeMessage } from "./utils/app.config";
 import { commentRoutes } from "./routes/comment.routes";
+import { ShutdownHandler } from "./utils/shutdown-handler";
+
 const app = new Elysia()
   .use(swagger(swaggerConfig))
   .get("/", () => welcomeMessage)
@@ -12,3 +14,6 @@ console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
 console.log('Comments API: http://localhost:5454/comments - Done by JuanBap');
+
+//Shutdown of the server in a appropiate way
+ShutdownHandler.setupSignalHandlers(app);
